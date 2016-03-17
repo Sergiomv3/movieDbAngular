c6a81c7aa4a1bb8bea78ae64ea89faaf49caae25
smartivi <sergiomartinvilchez@gmail.com>
2016-03-17 13:40:54 +0100

    HTTP get Service

--- scripts/controllers/MoviesController.js
+++ scripts/controllers/MoviesController.js
@@ -3,18 +3,19 @@
  */
 angular.module("MovieDbApp").controller("MoviesController",
     ["$scope","$http", function($scope,$http){
-
+        $scope.error = false;
         $scope.movies = [];
         $scope.loading = true;
         // recuperamos del servidor la lista de peliculas
         // mediante HTTP, inyectando como dependencia, crearemos un http falso
         $http.get("/api/movies/").then(function(data){
             console.log("Resultado de GET: ",data);
-            $scope.movies = data;
+            $scope.movies = data['data'];
             $scope.loading = false;
         }, function(err){
             console.log("Error while downloading data");
             $scope.loading = false;
+            $scope.error = true;
         }); // devuelve una promesa  (.then)
     }]
 );
